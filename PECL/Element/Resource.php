5345b54e49aade70981ccb522f5cbb5eb139efd0
Hartmut Holzgraefe <hholzgra@php.net>
2006-02-17 19:01:08 +0000

    build system fix (for out-of-band configure, needed by pear installer)
    git-svn-id: http://svn.php.net/repository/pear/packages/CodeGen_PECL/trunk@207371 c90b9560-bf6c-de11-be94-00142212c4b1

--- PECL/Element/Resource.php
+++ PECL/Element/Resource.php
@@ -292,7 +292,7 @@ le_{$this->name} = zend_register_list_destructors_ex({$this->name}_dtor,
      * @return string autoconf code snippet
      */
     function configm4($extension_name) {
-        return "  AC_CHECK_TYPE(".$this->getPayload()." *, [], [AC_MSG_ERROR(required payload type for resource ".$this->getName()." not found)], [#include \"php_{$extension_name}.h\"])\n";
+        return "  AC_CHECK_TYPE(".$this->getPayload()." *, [], [AC_MSG_ERROR(required payload type for resource ".$this->getName()." not found)], [#include \"\$srcdir/php_{$extension_name}.h\"])\n";
     }
 
 
