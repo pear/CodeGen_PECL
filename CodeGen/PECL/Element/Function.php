9c0bace0741d3f9bf612455b50ea995f4f7fbff8
robmeek <github@robmeek.com>
2013-01-09 23:49:30 +0100

    Fix 2nd typo to allow use of stream parameter type

--- CodeGen/PECL/Element/Function.php
+++ CodeGen/PECL/Element/Function.php
@@ -1194,7 +1194,7 @@ class CodeGen_PECL_Element_Function
                         $argString   .= "r";
                         $var_decl    .= "    zval * {$name}_zval = NULL;\n";
                         $var_decl    .= "    php_stream * $name = NULL;\n";
-                        $postProcess .= "    php_stream_from_zval($name, &_z$name);\n"; 
+                        $postProcess .= "    php_stream_from_zval($name, &{$name}_zval);\n";
                         break;
 
                     case "callback": 
