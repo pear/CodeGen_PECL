fabe2241a21d419f95eb96b384dd12795559a22b
Rouven Weßling <me@rouvenwessling.de>
2013-04-09 01:09:24 +0200

    Update PHP license to 3.01.

--- CodeGen/PECL/Dependency/Platform.php
+++ CodeGen/PECL/Dependency/Platform.php
@@ -4,9 +4,9 @@
  *
  * PHP versions 5
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
  * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
@@ -14,7 +14,7 @@
  * @package    CodeGen_PECL
  * @author     Hartmut Holzgraefe <hartmut@php.net>
  * @copyright  2005-2008 Hartmut Holzgraefe
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @link       http://pear.php.net/package/CodeGen_PECL
  */
 
@@ -30,7 +30,7 @@ require_once "CodeGen/Tools/Platform.php";
  * @package    CodeGen_PECL
  * @author     Hartmut Holzgraefe <hartmut@php.net>
  * @copyright  2005-2008 Hartmut Holzgraefe
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version    Release: @package_version@
  * @link       http://pear.php.net/package/CodeGen_PECL
  */
